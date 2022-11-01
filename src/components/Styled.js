a80e2a28cfa0cdcdb60f9102dbddd7d48ec078d3
MorthenJo <MorthenJo.kodehode@gmail.com>
2022-11-01 10:18:44 +0100

    trying again

---
+++ src/components/Styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+  margin: 5% 10%;
+  border: red solid 1px;
+`;
+
+export const Logo = styled.img`
+  object-fit: contain;
+  width: 30%;
+  margin: 0 auto;
+  display: block;
+`;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,11 @@
+import { Box, Logo } from "../components/Styled";
+import Quantfolio from "../pages/quantfolio.png";
 const LandingPage = () => {
   return (
-    <>
+    <Box>
+      <Logo src={Quantfolio} alt="quantfol.io" />
       <h1>bruh</h1>
-    </>
+    </Box>
   );
 };
 
